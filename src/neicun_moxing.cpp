ba8ee7e166cc62b09b25873973bd0fd4f4c548e4
liverfans-z <1017027546@qq.com>
2022-08-31 19:48:03 +0800

    修改

--- src/neicun_moxing.cpp
+++ src/neicun_moxing.cpp
@@ -4,8 +4,16 @@ using namespace std;
 int *fun()
 {
     // 使用new关键字，在堆区创建对象
+    // 利用new创建的数据，会返回该数据对应的类型的指针
     int *p = new int(10);
     return p;
+}
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main()
@@ -13,8 +21,49 @@ int main()
     int *p = fun();
     cout << *p << endl;
     cout << *p << endl;
-    cout << *p << endl;
-    cout << *p << endl;
+
+    // new一个数组
+    int *arr = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        // 给每个元素赋值
+        arr[i] = i + 100;
+    }
+
+    // 读取每个元素
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "元素:" << arr[i] << endl;
+    }
+
+    // 释放对象
+    delete p;
+    delete[] arr;
+
+    // --------------------------------------------
+
+    int aaa = 20;
+    int ccc = 55;
+    cout << "aaa1:" << aaa << endl; // 20
+    int &b = aaa;
+    // 这是赋值操作，不是更改引用，所以是可以的
+    b = ccc;
+
+    cout << "b:" << b << endl;      // 2000
+    cout << "aaa2:" << aaa << endl; // 2000
+
+    // --------------------------------------------
+
+    int s1 = 77;
+    int s2 = 88;
+    cout << "s1:" << s1 << endl; // 77
+    cout << "s2:" << s2 << endl; // 88
+    swap(s1, s2);
+    cout << "s1:" << s1 << endl; // 88
+    cout << "s2:" << s2 << endl; // 77
+
+    // --------------------------------------------
+
     system("pause");
     return 0;
 }
---
+++ src/yinyong.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int &test01()
+{
+    static int ccc = 10;
+    return ccc;
+}
+
+int main()
+{
+    // int ref，这样写，就表示定义一个整形的ref，是一个明确的值
+    // int &ref，这样写，就表示定义一个引用ref，是一个地址，指向test01的返回值
+    // 这两种的区别一定记清楚
+    int &ref = test01();
+    cout << ref << endl; // 10
+    cout << ref << endl; // 10
+
+    test01() = 999;
+    cout << ref << endl; // 999
+    cout << ref << endl; // 999
+
+    int aaa = 100;
+    const int * p = &aaa;
+    cout << *p << endl;
+
+    system("pause");
+    return 0;
+}
